8f57aab2212fa42cea09d622eef6377ca5abf3c4
Miika Halmela <miika.halmela@gmail.com>
2023-09-07 19:47:41 +0300

    changed text for printing option

--- prompts.py
+++ prompts.py
@@ -1,5 +1,5 @@
 def ui_action():
-    selection = input("\n1) add new book \n2) print current \nQ) exit\nselect: ")
+    selection = input("\n1) add new book \n2) print current in ascending order \nQ) exit\nselect: ")
     return selection
 
 
