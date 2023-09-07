a7cd2fe13b6b54038b5525f9bbb5ee15a5cfb93c
Miika Halmela <miika.halmela@gmail.com>
2023-09-07 18:36:10 +0300

    created library class for storing books read from file

---
+++ Library.py
@@ -0,0 +1,15 @@
+from fileHandler import FileHandler
+
+
+class Library:
+
+    def __init__(self, data_source):
+        self.fileHandler = FileHandler(data_source)
+        self.books = self.fileHandler.read_data()
+
+    def get_books(self):
+        return self.books
+
+    def add_book(self, book):
+        self.books.append(book)
+        self.fileHandler.write_data(self.books)
