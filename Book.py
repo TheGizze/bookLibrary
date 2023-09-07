e5f23c4b7f2a710186ae561411baffb7e3a120f0
Miika Halmela <miika.halmela@gmail.com>
2023-09-07 20:52:13 +0300

    created book object

---
+++ Book.py
@@ -0,0 +1,9 @@
+class Book:
+
+    def __init__(self, name, author, isbn, pub_year):
+        self.name = name
+        self.author = author
+        self.isbn = isbn
+        self.pub_year = pub_year
+
+
