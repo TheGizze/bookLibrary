8f1136b2355cc202f40f19f549f34e970ff334c2
Miika Halmela <miika.halmela@gmail.com>
2023-09-07 18:54:11 +0300

    created add book function to actions

--- actions.py
+++ actions.py
@@ -2,3 +2,12 @@ def print_books(library):
     print("")  # prints emptyline before printing books
     for book in library.get_books():
         print(book)
+
+
+def add_book(library):
+    book_name = input("\nbook name: ")
+    book_author = input("author name: ")
+    book_isbn = input("ISBN: ")
+    book_year = input("publishing year: ")
+    book = "{0}/{1}/{2}/{3}".format(book_name, book_author, book_isbn, book_year)
+    library.add_book(book)
